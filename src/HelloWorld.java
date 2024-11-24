177c6f98aa402d70bb939880f1d03d293ceb07d9
CoderSivasakthi <sivasakthi83974@gmail.com>
2024-11-25 01:09:18 +0530

    My 1st Hello world Program at Day 0

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("Hello All I'm Siva ^_^");
+        for(int i=0;i<10;i++){
+            System.out.println(i+1+" I love Java for coding");
+        }
+        System.out.println("Day 0 Completed");
+    }
+}
