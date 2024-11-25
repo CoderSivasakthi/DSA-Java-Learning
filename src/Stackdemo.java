a313db5ecee105773d0a29cd3b25abdbd16f6583
CoderSivasakthi <sivasakthi83974@gmail.com>
2024-11-25 23:16:56 +0530

    day 2 Stack implementation of inbuild methods

--- src/Stackdemo.java
+++ src/Stackdemo.java
@@ -1,5 +1,28 @@
+import java.util.Stack;
+
 public class Stackdemo {
     public static void main(String args[]){
+        Stack<Integer> stack = new Stack<>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println(stack);
+        stack.pop();
+        System.out.println(stack);
 
+        for(int i=0;i<10;i++){
+            stack.push(i);
+        }
+        System.out.println(stack);
+
+        for (int i=5;i>0;i--){
+            stack.pop();
+        }
+        System.out.println(stack);
+
+        Stack<String>stacks = new Stack<>();
+        stacks.push("today i completed stack push and pop inbuild methods");
+
+        System.out.println(stacks +" "+ stack);
     }
 }
